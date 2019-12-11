46bf5dfb56421ce187455bf0edc3d9552937c57e
LavrL <llembersk@gmail.com>
2019-12-11 17:35:53 +0200

    changed to HTTPS

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const DEFAULT_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
-export const MOVIE_API = "http://www.omdbapi.com/?s=batman&apikey=5d508a92";
+export const MOVIE_API = "https://www.omdbapi.com/?s=batman&apikey=5d508a92";
 
 
